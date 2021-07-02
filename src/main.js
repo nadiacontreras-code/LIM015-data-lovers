44cd585d91c91523632278bddd9981d787047afa
Mariperu <maritzarg2020@gmail.com>
2021-07-01 21:08:35 -0500

    Mostrando num, name, pic

--- src/main.js
+++ src/main.js
@@ -2,18 +2,31 @@
 //Importando de data.js
 import { example } from './data.js';
 import { anotherExample } from './data.js';
-import { pokemon } from './data.js';
 
 
 //BASE DE DATOS, carpeta DATA
 import data from './data/pokemon/pokemon.js';
 
-const datas = data;
-console.log(example, data, anotherExample)
+let pokemonNumber = "";
+let pokemonName ="";
 
-//let dataPokemon = data
+//Extrayendo num, name, img (en base a num)
+  data.pokemon.map(function(elem){
+  pokemonNumber = elem.num;
+  pokemonName = elem.name;
+  
+
+  let allNumbers = document.createElement("p");
+  allNumbers.innerHTML = pokemonNumber;
+  document.getElementById("allNumbers").appendChild(allNumbers);
 
 
+  let allNames = document.createElement("p");
+  allNames.innerHTML = pokemonName;
+  document.getElementById("allNames").appendChild(allNames);
 
+  let allPics = document.createElement("p");
+  allPics.innerHTML = '<img src=\'http://www.serebii.net/pokemongo/pokemon/'+ pokemonNumber+'.png\'>';
+  document.getElementById("allPics").appendChild(allPics);
 
-
+})
