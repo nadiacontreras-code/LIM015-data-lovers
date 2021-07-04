a42345cab5dd169299f64775118f44a1d286156c
Mariperu <maritzarg2020@gmail.com>
2021-07-04 12:31:01 -0500

    com

--- src/main.js
+++ src/main.js
@@ -15,18 +15,19 @@ let pokemonName ="";
   pokemonNumber = elem.num;
   pokemonName = elem.name;
   
-
   let allNumbers = document.createElement("p");
   allNumbers.innerHTML = pokemonNumber;
   document.getElementById("allNumbers").appendChild(allNumbers);
-
+  
 
   let allNames = document.createElement("p");
   allNames.innerHTML = pokemonName;
   document.getElementById("allNames").appendChild(allNames);
+  
 
   let allPics = document.createElement("p");
   allPics.innerHTML = '<img src=\'http://www.serebii.net/pokemongo/pokemon/'+ pokemonNumber+'.png\'>';
   document.getElementById("allPics").appendChild(allPics);
+  //document.getElementById("allPics").class()= "carouselPic";
 
 })
