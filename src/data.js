92f6f09211aa95d73e30fb03b90bc62cbc766ef6
Mariperu <maritzarg2020@gmail.com>
2021-07-06 10:02:34 -0500

    actualizando

--- src/data.js
+++ src/data.js
@@ -1,13 +1,35 @@
 //FUNCIONALIDAD
 
+//export const example = function sumar(num1, num2) {//RESUMIENDO
+export const example = (num1, num2) => {
+  let resultado
+  resultado = num1 + num2;
+  return resultado;
+  //console.log("La suma es: "+ resultado)
+};
+
+
+export const filterType = (data, typeChoose) => {
+  const typeOfPokemon = data.filter(
+    pokemon => pokemon.type.indexOf(typeChoose.toLowerCase()) > -1,
+  )
+  return typeOfPokemon;
+};
+
+
+/*
+const filterByGeneration = (data, valor) => {
+  const filterPokemonByGeneration = data.filter(
+    pokemon => pokemon.generation.name === valor,
+  );
+  return filterPokemonByGeneration;
+};
+*/
+
 
 /*
 export const example = () => {
-  let pokemonNumber = "";
-  data.pokemon.map(function(elem){
-    pokemonNumber = elem.num;
-    //pokemonName = elem.name; 
-    console.log(pokemonNumber)
+  
  })
  return pokemonNumber;
  
