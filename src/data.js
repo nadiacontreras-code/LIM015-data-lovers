2271909cb67153828d98f3e79006ec0e1e96f0be
Mariperu <maritzarg2020@gmail.com>
2021-07-12 11:16:39 -0500

    Actualizando

--- src/data.js
+++ src/data.js
@@ -41,8 +41,31 @@ export const alphabeticalOrder = (selectOrder, dataName) => {
 //console.log(pokemonOrder);
 
 
-//FUNCION FILTRAR POR TIPO  ((aun no funciona, mejorar ))
+//FUNCION FILTRAR POR TIPO  
+//TERCER INTENTO
+export const typeFilter = (selectType, dataType) => {
+  const pokemonTypeFilter = selectType.filter((pok) => {
+    pok.type.indexOf(dataType) > -1
+  });
+  return pokemonTypeFilter;
+};
 
+//SEGUNDO INTENTO
+/*export const typeFilter = (selectorType, dataType) => {
+  let prueba3 = dataType.filter((item) => {
+*/
+/*for (let i = 0; i < item.type.length; i++) {
+  let comparation = item.type[i];
+  console.log(comparation);*/
+/*if (item.type.indexOf(selectorType) == 0 || item.type.indexOf(selectorType) == 1) {
+  return true;
+}
+return false;
+})
+console.log(prueba3);
+}*/
+
+/*PRIMER INTENTO
 export const typeFilter = (selectType, dataType) => {
   let pokemonTypeFilter;
   if (selectType == "poison") {
@@ -54,7 +77,7 @@ export const typeFilter = (selectType, dataType) => {
     return " "
   }
 }
-
+*/
 //indexOf()método devuelve posición de primera aparición valor especificado
 //indexOf() si no coindice devuelve -1
 //const pokemonTypeFilter = selectType.filter(pokemon => pokemon.type.indexOf(dataType.toLowerCase()) > -1, );
